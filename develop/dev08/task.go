a3b0427732fb08962a9b562021a14476efea2a4f
romaniukae <arseniy.romaniuk@pimpay.ru>
2023-04-13 03:24:01 +0300

    tasks complete

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,113 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
+	goPs "github.com/mitchellh/go-ps"
+)
+
+/*
+	8. 	Minishell
+		Необходимо реализовать свой собственный UNIX-шелл-утилиту с поддержкой ряда простейших команд:
+
+		- cd <args> - смена директории (в качестве аргумента могут быть то-то и то)
+		- pwd - показать путь до текущего каталога
+		- echo <args> - вывод аргумента в STDOUT
+		- kill <args> - "убить" процесс, переданный в качестве аргумента (пример: такой-то пример)
+		- ps - выводит общую информацию по запущенным процессам в формате *такой-то формат*
+*/
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		request := strings.Split(scanner.Text(), " ")
+		switch request[0] {
+		case "cd":
+			cd(request)
+		case "pwd":
+			pwd(request)
+		case "echo":
+			echo(request)
+		case "kill":
+			kill(request)
+		case "ps":
+			ps(request)
+		}
+	}
+}
+
+func cd(request []string) {
+	switch len(request) {
+	case 1:
+		fmt.Fprintln(os.Stderr, "where dir?")
+	case 2:
+		err := os.Chdir(request[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "too many arguments")
+	}
+}
+
+func pwd(request []string) {
+	if len(request) == 1 {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		} else {
+			fmt.Println(path)
+		}
+	} else {
+		fmt.Fprintln(os.Stderr, "too many arguments")
+	}
+}
+
+func echo(request []string) {
+	for i := 1; i < len(request); i++ {
+		fmt.Printf("%s ", request[i])
+	}
+	fmt.Println()
+}
+
+func kill(request []string) {
+	if len(request) == 1 {
+		fmt.Fprintln(os.Stderr, "what?")
+		return
+	}
+	if len(request) > 2 {
+		fmt.Fprintln(os.Stderr, "too many arguments")
+		return
+	}
+	pid, err := strconv.Atoi(request[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	err = process.Kill()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+}
+
+func ps(request []string) {
+	if len(request) != 1 {
+		fmt.Fprintln(os.Stderr, "too many arguments")
+		return
+	}
+	sliceProc, _ := goPs.Processes()
+
+	for _, proc := range sliceProc {
+		fmt.Printf("Process name: %v process id: %v\n", proc.Executable(), proc.Pid())
+	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module lazzytchik/l2
 
 go 1.20
 
-require github.com/beevik/ntp v0.3.0
+require (
+	github.com/beevik/ntp v0.3.0
+	github.com/mitchellh/go-ps v1.0.0
+	github.com/pborman/getopt v1.1.0
+)
 
 require (
 	github.com/stretchr/testify v1.8.2 // indirect
